db26c64d590263ee92a6e335222c96f97d5810e2
Chukwuemeka chukwurah <emekaemmanuel045@gmail.com>
2022-09-25 12:01:02 +0100

    meeting routes declarations (#2)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express from "express";
 import { globalErrorHandler, routeNotFoundHandler } from "./middlewares/index";
+import { meetingRoute } from "./routes/meeting";
 import { tutorRoute } from "./routes/tutor";
 
 export const app = express();
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
   res.send("Kola and Kruse cooked this🧑🏾‍🍳");
 });
 
+app.use("/api/v1/meetings", meetingRoute);
 app.use("/api/v1/tutors", tutorRoute);
 
 app.use(routeNotFoundHandler);
