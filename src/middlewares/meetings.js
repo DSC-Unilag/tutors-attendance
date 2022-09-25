f93d431afdfe1317bb50e15459ac4d59afd037d9
kolabdull <100072694+kolabdull@users.noreply.github.com>
2022-09-25 15:12:13 +0100

    tutor route declaration (#5)

--- src/middlewares/meetings.js
+++ src/middlewares/meetings.js
@@ -1,5 +1,8 @@
 import joi from "joi";
+import joiObjectId from "joi-objectid";
 import { parseJoiError } from "../utilities";
+
+joi.objectId = joiObjectId(joi);
 
 async function create(req, res, next) {
   const schema = joi.object().keys({
@@ -19,6 +22,32 @@ async function create(req, res, next) {
   }
 }
 
+export async function update(req, res, next) {
+  const paramsSchema = joi.object().keys({
+    id: joi.objectId().required(),
+  });
+  const bodySchema = joi.object().keys({
+    endDate: joi.date().optional().default(new Date()),
+  });
+  try {
+    req.body = await bodySchema.validateAsync(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    req.params = await paramsSchema.validateAsync(req.params, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    next();
+  } catch (err) {
+    const errors = parseJoiError(err);
+    res.status(422).json({ errors });
+  }
+}
+
 export const meetingMiddleware = {
   create,
+  update,
 };
